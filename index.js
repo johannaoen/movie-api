341022109dddb4b2a05fa8e5107b5b7919a7cb76
johannaoen <107009997+johannaoen@users.noreply.github.com>
2022-11-17 12:19:59 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,207 @@
+const express = require('express');
+const app = express();
+
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const uuid = require('uuid');
+
+
+
+fs = require('fs'),
+path = require('path');
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(morgan('common'));
+
+
+let users = [
+  {
+    id : 1,
+    name : 'Johanna',
+    favoriteMovies: ['Frozen']
+  },
+  {
+    id : 2,
+    name : 'Bob',
+    favoriteMovies: ['Harry Potter 1']
+  },
+]
+
+let movies = [
+  {
+    "Title": "Harry Potter 1",
+    "Description": "Yer a wizard Harry.",
+    "Genre": {
+      "Name": "Adventure",
+      "Description": "In film and television, adventure is a category of a narrative fiction or semi-fiction"
+    },
+    "Director": {
+      "Name": "J.K. Rowling",
+      "Bio" : "J.K Rowling Biography, Height, Weight, Age, Measurements, Net Worth, Family, Wiki & much more! .",
+      "Birth" : 1995,
+    },
+    "ImageUrl" : "jkrowling.jpg",
+    "Featured" : false
+  },
+
+  {
+    "Title": "Harry Potter 2",
+    "Description": "Yer a wizard Harry.",
+    "Genre": {
+      "Name": "Adventure",
+      "Description": "In film and television, adventure is a category of a narrative fiction or semi-fiction"
+    },
+    "Director": {
+      "Name": "J.K. Rowling",
+      "Bio" : "J.K Rowling Biography, Height, Weight, Age, Measurements, Net Worth, Family, Wiki & much more! .",
+      "Birth" : 1995,
+    },
+    "ImageUrl" : "jkrowling.jpg",
+    "Featured" : false
+  },
+
+  {
+    "Title": "Frozen",
+    "Description": "Ana and Elsa lose both of their parents in a tragic shipwreck are now tasked with ruling over the kingdom of Arendelle. However, they must navigate Elsa's destructive ice powers together.",
+    "Genre": {
+      "Name": "Family",
+      "Description": "In film and television, Family is a category of a narrative fiction or semi-fiction suitable for family and kids."
+    },
+    "Director": {
+      "Name": "John Doe",
+      "Bio" : "John Doe is an American writer, director and producer of film and television. He is best known for creating the movie series Frozen 1 and Frozen 2.",
+      "Birth" : 1961,
+    },
+    "ImageUrl" : "frozen.png",
+    "Featured" : false
+  }
+];
+
+
+
+app.get('/', (req, res) => {
+  // res.send('Welcome to my app!');
+  // responseText += '<small>Requested at: ' + req.requestTime + '</small>';
+  let responseText = "Welcome to myFlix app!";
+  res.send(responseText);
+});
+//Gets data on ALL movies
+app.get('/', (req, res) => {
+  // res.send('Welcome to my app!');
+  // responseText += '<small>Requested at: ' + req.requestTime + '</small>';
+  let responseText = "Welcome to myFlix app!";
+  res.send(responseText);
+});
+//Gets data on ALL movies
+app.get('/movies', (req, res) => {
+res.status(200).json(movies);
+});
+//Gets data about single movie by title
+app.get('/movies/:title', (req, res) => {
+  const {title} =  req.params;
+  const movie = movies.find(movie => movie.Title === title);
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send("no such movie")
+  }
+})
+
+// READ genre by name
+app.get('/movies/genre/:genreName', (req, res) => {
+  const { genreName } =  req.params;
+  const genre = movies.find(movie => movie.Genre.Name === genreName).Genre; // the the .Genre will enable it return just the genre object
+
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send("no such genre")
+  }
+})
+
+//get data about a director by name (bio, birth year, death yr)
+app.get('/movies/director/:directorName', (req, res) => {
+  const { directorName } =  req.params;
+  const director = movies.find(movie => movie.Director.Name === directorName).Director; // the the .Genre will enable it return just the genre object
+
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send("no such director")
+  }
+})
+//allow new users to register
+app.post('/users', (req, res) => {
+let newUser = req.body;
+
+if (newUser.name){
+  newUser.id = uuid.v4();
+  users.push(newUser);
+  res.status(201).send(newUser);
+}else {
+  res.status(400).send('New user requires name');
+}
+});
+//allow user to update info (username)
+app.put('/users/:name/:username', (req, res) => {
+  let user = users.find((user) => {return user.name === req.params.name });
+
+  if (user) {
+    res.status(201).send('User with the name' + req.params.name + 'changed username to' + req.params.username);
+  } else {
+    res.status(404).send('User with the name' + req.params.name + 'was not found.');
+  }
+});
+//allow users to ADD a movie to their list of favorites(showing text that move has been added)
+app.post('/users/:name/:movieTitle', (req, res) => {
+	const { name, movieTitle } = req.params;
+
+	let user = users.find((user) => user.name == name);
+
+	if (user) {
+		user.favoriteMovies.push(movieTitle);
+		res.status(200).send(
+			`Movie has been added to user array`
+		);
+	} else {
+		res.status(400).send('User not found');
+	}
+});
+
+//allow users to DELETE a movie from their list of favorites (showing text)
+app.delete('/users/:name/:movieTitle', (req, res) => {
+	const { name, movieTitle } = req.params;
+
+	let user = users.find((user) => user.name == name);
+
+	if (user) {
+		user.favoriteMovies = user.favoriteMovies.filter(
+			(title) => title !== movieTitle
+		);
+		res.status(200).send(
+			`Movie has been removed from user array`
+		);
+	} else {
+		res.status(400).send('User not found');
+	}
+});
+
+ //allow existing users to deregister(text that user email has been removed)
+ app.delete('/users/:name', (req, res) => {
+	const { name } = req.params;
+
+	let user = users.find((user) => user.name == name);
+
+	if (user) {
+		res.status(200).send(`User has been removed from the database`);
+	} else {
+		res.status(400).send('User not found');
+	}
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
