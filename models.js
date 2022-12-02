bc210f05eafbb115ecfc9eb70b47935b7177b08b
johannaoen <107009997+johannaoen@users.noreply.github.com>
2022-12-01 16:15:11 -0800

    syntax correction

--- models.js
+++ models.js
@@ -18,19 +18,19 @@ let movieSchema = mongoose.Schema({
 });
 
 let userSchema = mongoose.Schema({
-  Username: {type: String, required: true},
-  Password: {type: String, required: true},
-  Email: {type: String, required: true},
-  Birthday: Date,
-  FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref:'Movie'}]
+  username: {type: String, required: true},
+  password: {type: String, required: true},
+  email: {type: String, required: true},
+  birthday: Date,
+  favoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref:'Movie'}]
 });
 
-userSchema.statics.hashPassword = (password) => {
-  return bcrypt.hashSync(password, 10);
-  };
-  userSchema.methods.validatePassword = function(password) {
-    return bcrypt.compareSync(password, this.Password);
-  };
+//userSchema.statics.hashPassword = (password) => {
+ // return bcrypt.hashSync(password, 10);
+ // };
+ // userSchema.methods.validatePassword = function(password) {
+    //return bcrypt.compareSync(password, this.Password);
+  //};
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
