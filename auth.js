a4604cc104fe9409f6f49765b7898cee17624e95
johannaoen <107009997+johannaoen@users.noreply.github.com>
2022-12-09 10:42:49 -0800

    syntax correction

--- auth.js
+++ auth.js
@@ -6,7 +6,7 @@ passport = require('passport');
 require('./passport'); //Your local passport file
 
 let generateJWTToken = (user) => {
-    return jwt.sign(User, jwtSecret, {
+    return jwt.sign(user, jwtSecret, {
         subject: user.Username, // This is the username you're encoding in the JWT
         expiresIn: '7d', //This specifies that the token will expire in 7 days
         algorithm: 'HS256'//This is the algorithm used to "sign" or encode the values of the JWT
