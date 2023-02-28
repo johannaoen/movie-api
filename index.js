acc105bdc15a49838bf4ec8f50fd419a24129dce
johannaoen <107009997+johannaoen@users.noreply.github.com>
2023-02-28 15:59:28 -0800

    removed authentication for movie endpoint

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get('/', (req, res) => {
 });
 
 //Gets data on ALL movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies'), (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
